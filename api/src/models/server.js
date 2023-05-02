56ddc34ec47e2a2891fde29018acd267e2df6ac8
ruldiaz <rulhdiaz@gmail.com>
2023-05-02 06:24:50 -0600

    server

--- api/src/models/server.js
+++ api/src/models/server.js
@@ -5,18 +5,44 @@ const express = require('express');
 class Server {
     constructor(){
         this.app = express();
-        this.routes();
         this.PORT = process.env.PORT;
+        
+        //Middlewares
+        this.middlewares();
+
+        //Routes
+        this.routes();
+    }
+
+    middlewares(){
+        // public directory
+        this.app.use( express.static('public'));
     }
 
     routes(){
-        this.app.get('/', (req, res)=>{
-            res.send('Hello world');
-        });
-        
-        this.app.get('*', (req, res)=>{
-            res.send('404 | Page not found');
+        this.app.get('/api', (req, res)=>{
+            res.status(200).json({
+                msg: 'get API'
+            });
         })
+
+        this.app.put('/api', (req, res)=>{
+            res.status(200).json({
+                msg: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res)=>{
+            res.status(200).json({
+                msg: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res)=>{
+            res.status(200).json({
+                msg: 'delete API'
+            });
+        });
     }
 
     listen(){
