f259e5795227d1a17247a1633660b18b4f707c17
ruldiaz <rulhdiaz@gmail.com>
2023-05-12 22:02:10 -0600

    home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsers } from '../actions';
 
-function Home(){
+export default function Home(){
+
+    const dispatch = useDispatch();
+    const allUsers = useSelector( state => state.users );
+
+    useEffect(()=>{
+        dispatch( getUsers() );
+    }, [dispatch])
+
+    function handleClick(e){
+        e.preventDefault();
+        dispatch( getUsers() );
+    };
 
     return (
         <div>
-            <h1>Hello World !</h1>
+            <Link to='/user'>Create User</Link>
+            <h1>POS</h1>
+            <button onClick={handleClick}>Refresh</button>
+            <div>
+                <select>
+                    <option value='asc'>Ascending</option>
+                    <option value='desc'>Descending</option>
+                </select>
+                <select>
+                    <option value='all'>All</option>
+                    <option value='user'>User Role</option>
+                    <option value='admin'>Admin Role</option>
+                    <option value='sales'>Sales Role</option>
+                </select>
+            </div>
         </div>
     );
 
 };
-
-export default Home;
