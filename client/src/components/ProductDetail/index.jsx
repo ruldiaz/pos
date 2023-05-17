264b8f303e1f1c2590266ba16081580e376c308e
ruldiaz <rulhdiaz@gmail.com>
2023-05-17 07:38:37 -0600

    detail

--- client/src/components/ProductDetail/index.jsx
+++ client/src/components/ProductDetail/index.jsx
@@ -4,7 +4,10 @@ import './styles.css';
 export default function ProductDetail(){
     return (
         <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
-
+            <div className='flex justify-between items-center'>
+                <h2 className='font-medium text-xl'>Detail</h2>
+                <div>x</div>
+            </div>
         </aside>
     );
 };
