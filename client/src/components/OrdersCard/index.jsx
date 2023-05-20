08b4d01ddde8df2eaccff04b5269d3b71a65fa6d
ruldiaz <rulhdiaz@gmail.com>
2023-05-19 22:03:45 -0600

    orders card update

--- client/src/components/OrdersCard/index.jsx
+++ client/src/components/OrdersCard/index.jsx
@@ -7,17 +7,17 @@ export default function OrdersCard(props){
 
     return (
         <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80'>
-            <p className='flex justify-between w-full'>
-                <div className='flex flex-col'>
+            <div className='flex justify-between w-full'>
+                <p className='flex flex-col'>
                     <span className='font-light'>{'18.05.2023'}</span>
                     <span className='font-light'>{totalProducts} articles </span>
-                </div>
-                <div>
+                </p>
+                <p className='flex items-center gap-2'>
                     <span className='font-medium text-2xl'>$ {totalPrice}</span>
                     <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'></ChevronRightIcon>
-                </div>
+                </p>
                 
-            </p>
+            </div>
         </div>
     );
 };
