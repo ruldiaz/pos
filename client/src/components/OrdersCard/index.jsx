dde044ff3f3e1c478cd22358ff1a2c173698f3ca
ruldiaz <rulhdiaz@gmail.com>
2023-05-19 21:50:08 -0600

    update orders card

--- client/src/components/OrdersCard/index.jsx
+++ client/src/components/OrdersCard/index.jsx
@@ -6,11 +6,14 @@ export default function OrdersCard(props){
     const { totalPrice, totalProducts } = props;
 
     return (
-        <div className='flex justify-between items-center mb-3 border border-black'>
-            <p>
-                <span>{'18.05.2023'}</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
+        <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80 mb-4'>
+            <p className='flex justify-between'>
+                <div className='flex flex-col'>
+                    <span className='font-light'>{'18.05.2023'}</span>
+                    <span className='font-light'>{totalProducts}</span>
+                </div>
+                
+                <span className='font-medium text-2xl'>$ {totalPrice}</span>
             </p>
         </div>
     );
